7e5b1881ef7465726bfd071dafcfb9d0039426e7
GunPKate <gunPK>
2024-03-12 10:44:14 +0700

    multiple root router

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+// localhost___/about
+import styles from "./page.module.css"
+
+export default function Dashboard(){
+    return (<>
+        <h1 className={styles.headerSalmon}>Dashboard</h1>
+    </>)
+}
